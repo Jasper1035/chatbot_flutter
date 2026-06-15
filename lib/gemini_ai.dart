9149eff592c0064cc310f9f68b4698d6c0929df1
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-06-15 20:56:20 +0100

    done .
    add the groq api it worked fine..

--- lib/gemini_ai.dart
+++ lib/gemini_ai.dart
@@ -1,7 +1,10 @@
 import 'package:chatbot_flutter/model.dart';
 import 'package:flutter/material.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/intl.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class GeminiAi extends StatefulWidget {
   const GeminiAi({super.key});
@@ -13,32 +16,72 @@ class GeminiAi extends StatefulWidget {
 class _GeminiAiState extends State<GeminiAi> {
   TextEditingController promptController = TextEditingController();
 
-  static const apiKey = '';
-  final model = GenerativeModel(model: 'Gemini-Pro', apiKey: apiKey);
+  static final apiKey = dotenv.env['groq_api_key'];
 
   final List<ModelMessage> prompt = [];
 
+  // Future<void> sendMessage() async {
+  //   final message = promptController.text;
+  //   //for prompt
+  //   setState(() {
+  //     prompt.add(
+  //       ModelMessage(isPrompt: true, message: message, time: DateTime.now()),
+  //     );
+  //   });
+  //   //for response
+
+  //   final content = [Content.text(message)];
+  //   final response = await model.generateContent(content);
+  //   setState(() {
+  //     prompt.add(
+  //       ModelMessage(
+  //         isPrompt: false,
+  //         message: response.text ?? '',
+  //         time: DateTime.now(),
+  //       ),
+  //     );
+  //   });
+  // }
   Future<void> sendMessage() async {
-    final message = promptController.text;
-    //for prompt
+    final message = promptController.text.trim();
+
+    if (message.isEmpty) return;
+
     setState(() {
       prompt.add(
         ModelMessage(isPrompt: true, message: message, time: DateTime.now()),
       );
     });
-    //for response
 
-    final content = [Content.text(message)];
-    final response = await model.generateContent(content);
-    setState(() {
-      prompt.add(
-        ModelMessage(
-          isPrompt: false,
-          message: response.text ?? '',
-          time: DateTime.now(),
-        ),
+    promptController.clear();
+
+    try {
+      final response = await http.post(
+        Uri.parse('https://api.groq.com/openai/v1/chat/completions'),
+        headers: {
+          'Authorization': 'Bearer $apiKey',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          "model": "llama-3.1-8b-instant",
+          "messages": [
+            {"role": "user", "content": message},
+          ],
+        }),
       );
-    });
+
+      final data = jsonDecode(response.body);
+
+      final reply = data['choices'][0]['message']['content'];
+
+      setState(() {
+        prompt.add(
+          ModelMessage(isPrompt: false, message: reply, time: DateTime.now()),
+        );
+      });
+    } catch (e) {
+      print("ERROR: $e");
+    }
   }
 
   @override
@@ -86,8 +129,8 @@ class _GeminiAiState extends State<GeminiAi> {
                 // SizedBox(width: 20),
                 Spacer(),
                 GestureDetector(
-                  onTap: () {
-                    sendMessage();
+                  onTap: () async {
+                    await sendMessage();
                   },
                   child: CircleAvatar(
                     radius: 30,
